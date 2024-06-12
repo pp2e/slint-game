980d03bc8e4159c064ab9981d530aa1e83364927
New me! <enuromskiy@gmaii.com>
2024-06-12 13:03:08 +0300

    fix web

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 slint::include_modules!();
 
-#[cfg_attr(target_arch = "wasm32",
-           wasm_bindgen::prelude::wasm_bindgen(start))]
-pub fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub fn main() {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(all(debug_assertions, target_arch = "wasm32"))]
+    console_error_panic_hook::set_once();
+    
+    let ui = AppWindow::new().unwrap();
 
     ui.on_request_increase_value({
         let ui_handle = ui.as_weak();
@@ -13,5 +20,5 @@ pub fn main() -> Result<(), slint::PlatformError> {
         }
     });
 
-    ui.run()
+    ui.run().unwrap();
 }
